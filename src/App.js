4ba0967dbf04f65605e724fcf671a19523249884
kacperhaha <kacperhaha11@gmail.com>
2022-07-16 13:58:18 +0200

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const day = `${new Date().getDate()}`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="flex justify-center content-center flex-col items-center">
+          <p className="">Dzisiaj jest:</p>
+          {/* <p>{day}</p> */}
+        </div> 
     </div>
   );
 }
